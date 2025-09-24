850cf21885fad3c06c5d731c7f0bc7618a576b49
Movieguy3333 <Movieguy3333@gmail.com>
2025-09-24 08:38:44 -0400

    Setup routing

--- GreenBite/src/App.tsx
+++ GreenBite/src/App.tsx
@@ -1,12 +1,27 @@
-import MainTitle from './MainTitle.tsx'
-import './App.css'  
-import './index.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import "./index.css";
+import HomePage from "./features/ui/HomePage";
 function App() {
   return (
-    <div>
-      <MainTitle />
-    </div>
-  )
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        <Route index element={<HomePage />} />
+
+        <Route path="sign-up" element={<h1>Sign-up</h1>} />
+        <Route path="login" element={<h1>Login</h1>} />
+
+        <Route path="dashboard" element={<h1>Dashboard</h1>}>
+          <Route index element={<Navigate replace to="profile" />} />
+          <Route path="profile" element={<h1>profile</h1>} />
+          <Route path="add-meal" element={<h1>add meal</h1>} />
+        </Route>
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+      {/*    <Footer /> */}
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
